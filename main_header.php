ba2e2b0cc0796c3b7586cab501643f2862260ac0
srinivaslancuius <srinivas@lanciussolutions.com>
2017-11-03 17:29:34 +0530

    chnages in main_header and footer files

--- main_header.php
+++ main_header.php
@@ -12,7 +12,7 @@
 			<div class="col-lg-6 col-md-6 col-sm-6">
 				<div class="topbar-right text-right">
 					 <ul class="list-inline">
-             <li> <i class="fa fa-phone"></i><?php echo $getSiteSettingsData['mobile'];?></li> 
+             <li> <i class="fa fa-phone"></i><a href="Tel:<?php echo $getSiteSettingsData['mobile']; ?>"><?php echo $getSiteSettingsData['mobile'];?></a></li> 
              <li><a target="_blank" href="<?php echo $getSiteSettingsData['fb_link'];?>"><i class="fa fa-facebook"></i></a></li>   
              <li><a target="_blank" href="<?php echo $getSiteSettingsData['twitter_link'];?>"><i class="fa fa-twitter"></i></a></li>   
              <li><a target="_blank" href="<?php echo $getSiteSettingsData['gplus_link'];?>"><i class="fa fa-youtube-play"></i></a></li>   
