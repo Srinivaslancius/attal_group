3f4681794d896656c8b18c8c1aa581bb0babf938
srinivaslancuius <srinivas@lanciussolutions.com>
2017-11-08 19:33:35 +0530

    chnages in projects file

--- manage_webmaster/projects.php
+++ manage_webmaster/projects.php
@@ -1,5 +1,5 @@
 <?php include_once 'admin_includes/main_header.php'; ?>
-<?php $sql = "SELECT * FROM projects";
+<?php $sql = "SELECT * FROM projects ORDER BY status";
   $getProjectsData = $conn->query($sql); $i=1; ?>
      <div class="site-content">
         <div class="panel panel-default panel-table">
